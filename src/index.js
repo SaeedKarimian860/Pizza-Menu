d09503936915590cd75dabab7c447318235f2242
SaeedKarimian860 <SGK12190@gmail.com>
2025-03-01 20:04:34 -0500

    Pizza Menu

--- src/index.js
+++ src/index.js
@@ -122,10 +122,10 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen && (
-      <div className="order">
-        <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-        <button className="btn">Order</button>
-      </div>
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
       )}
     </footer>
   );
