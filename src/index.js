12e9f28f15eed082c5bea2d47a9b7abaca6399d9
SaeedKarimian860 <SGK12190@gmail.com>
2025-03-02 23:32:33 -0500

    Pizza Menu

--- src/index.js
+++ src/index.js
@@ -140,7 +140,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <Order openHours={openHour} />
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
@@ -156,7 +156,7 @@ function Order(props) {
   return (
     <div className="order">
       <p>
-        We're open until {props.closeHours}:00. Come visit us or order online.
+        We're open until {props.closeHour}:00. Come visit us or order online.
       </p>
       <button className="btn">Order</button>
     </div>
